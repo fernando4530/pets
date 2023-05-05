8fb5cd144e328b41478b6c1d8a3b3a18697500e2
tebyortiz <tebyortiz@gmail.com>
2023-05-04 22:46:28 -0300

    catbreeds component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import axios from "axios";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Routes, Route } from "react-router-dom";
+import CatsBreeds from "./Components/CatsBreeds";
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
         <Container>
           <Nav className="me-auto">
             <NavDropdown title="GATOS" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Razas</NavDropdown.Item>
-              </NavDropdown>
+              <NavDropdown.Item href="/catsbreeds">Razas</NavDropdown.Item>
+            </NavDropdown>
 
             <NavDropdown title="PERROS" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Razas</NavDropdown.Item>
@@ -20,8 +21,11 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+      <Routes>
+        <Route path="/catsbreeds" element={<CatsBreeds />} />
+      </Routes>
     </div>
   );
 }
 
-export default App; 
+export default App;
