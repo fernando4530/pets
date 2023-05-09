98390f3f8ffb2d013e795d826904b8eb1931a22b
miki <ortizmichel390@gmail.com>
2023-05-09 01:57:58 -0300

    improved routing and rendering condition

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Routes, Route } from "react-router-dom";
-import CatsBreeds from "./Components/CatsBreeds";
+import CatsBreeds from "./components/CatsBreeds";
+import DogsBreeds from "./components/DogsBreeds";
 
 function App() {
   return (
@@ -16,13 +17,14 @@ function App() {
             </NavDropdown>
 
             <NavDropdown title="PERROS" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/breeds">Razas</NavDropdown.Item>
+              <NavDropdown.Item href="/dogsbreeds">Razas</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Container>
       </Navbar>
       <Routes>
         <Route path="/catsbreeds" element={<CatsBreeds />} />
+        <Route path="/dogsbreeds" element={<DogsBreeds />} />
       </Routes>
     </div>
   );
