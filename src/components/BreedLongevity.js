52563d780967a9702ea6087e07d95e8f0c414d7f
miki <ortizmichel390@gmail.com>
2023-05-23 17:45:46 -0300

    refactored component

--- src/components/BreedLongevity.js
+++ src/components/BreedLongevity.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Dropdown, DropdownButton, ListGroup, Row, Col } from "react-bootstrap";
+import {
+  Dropdown,
+  DropdownButton,
+  ListGroup,
+  Row,
+  Col,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 
 const BreedLongevity = () => {
   const [dogBreeds, setDogBreeds] = useState([]);
@@ -9,13 +17,63 @@ const BreedLongevity = () => {
   const [selectedCatAge, setSelectedCatAge] = useState("");
   const [filteredDogBreeds, setFilteredDogBreeds] = useState([]);
   const [filteredCatBreeds, setFilteredCatBreeds] = useState([]);
+  const [dogAgeAverage, setDogAgeAverage] = useState(0);
+  const [catAgeAverage, setCatAgeAverage] = useState(0);
+
+  const calculateDogAgeAverage = (age) => {
+    const filteredDogs = Object.values(dogBreeds).filter(
+      (breed) => breed.life_span === age
+    );
+    const totalDogs = filteredDogs.length;
+    let totalAge = 0;
+
+    filteredDogs.forEach((dog) => {
+      const lifeSpan = dog.life_span.split(" ");
+      const minAge = parseInt(lifeSpan[0]);
+      const maxAge = parseInt(lifeSpan[2]);
+      const average = (minAge + maxAge) / 2;
+      totalAge += average;
+    });
+
+    const averageAge = totalAge / totalDogs;
+    setDogAgeAverage(averageAge.toFixed(1));
+    console.log(
+      "Promedio de esperanza de vida de perros:",
+      averageAge.toFixed(1),
+      "años"
+    );
+  };
+
+  const calculateCatAgeAverage = (age) => {
+    const filteredCats = Object.values(catBreeds).filter(
+      (breed) => breed.life_span === age
+    );
+    const totalCats = filteredCats.length;
+    let totalAge = 0;
+
+    filteredCats.forEach((cat) => {
+      const lifeSpan = cat.life_span.split(" ");
+      const minAge = parseInt(lifeSpan[0]);
+      const maxAge = parseInt(lifeSpan[2]);
+      const average = (minAge + maxAge) / 2;
+      totalAge += average;
+    });
+
+    const averageAge = totalAge / totalCats;
+    setCatAgeAverage(averageAge.toFixed(1));
+    console.log(
+      "Promedio de esperanza de vida de gatos:",
+      averageAge.toFixed(1),
+      "años"
+    );
+  };
 
   useEffect(() => {
     const fetchDogBreeds = async () => {
       try {
         const response = await axios.get("https://api.thedogapi.com/v1/breeds");
         setDogBreeds(response.data);
-        console.log("Razas de(Dogs)buscadas:", response.data);
+        console.log("Razas de perros buscadas:", response.data);
       } catch (error) {
         console.error(error);
       }
@@ -25,7 +83,7 @@ const BreedLongevity = () => {
       try {
         const response = await axios.get("https://api.thecatapi.com/v1/breeds");
         setCatBreeds(response.data);
-        console.log("Razas de(Cats)buscadas:", response.data);
+        console.log("Razas de gatos buscadas:", response.data);
       } catch (error) {
         console.error(error);
       }
@@ -37,48 +95,72 @@ const BreedLongevity = () => {
 
   const handleDogAgeSelect = (age) => {
     setSelectedDogAge(age);
-    console.log("Edad(Dog)seleccionada:", age);
+    console.log("Edad (Perro) seleccionada:", age);
+    calculateDogAgeAverage(age);
   };
 
   const handleCatAgeSelect = (age) => {
     setSelectedCatAge(age);
-    console.log("Edad(Cat)seleccionada:", age);
+    console.log("Edad (Gato) seleccionada:", age);
+    calculateCatAgeAverage(age);
   };
 
   useEffect(() => {
     if (selectedDogAge) {
-      const filteredDogs = dogBreeds.filter(
+      const filteredDogs = Object.values(dogBreeds).filter(
         (breed) => breed.life_span === selectedDogAge
       );
       setFilteredDogBreeds(filteredDogs);
-      console.log("Razas(Dogs)filtradas:", filteredDogs);
+      console.log("Razas de perros filtradas:", filteredDogs);
+      calculateDogAgeAverage(selectedDogAge);
     } else {
-      setFilteredDogBreeds([]);
+      setFilteredDogBreeds({});
+      setDogAgeAverage(0);
     }
   }, [selectedDogAge, dogBreeds]);
 
   useEffect(() => {
     if (selectedCatAge) {
-      const filteredCats = catBreeds.filter(
+      const filteredCats = Object.values(catBreeds).filter(
         (breed) => breed.life_span === selectedCatAge
       );
       setFilteredCatBreeds(filteredCats);
-      console.log("Razas(cat)filtradas:", filteredCats);
+      console.log("Razas de gatos filtradas:", filteredCats);
+      calculateCatAgeAverage(selectedCatAge);
     } else {
-      setFilteredCatBreeds([]);
+      setFilteredCatBreeds({});
+      setCatAgeAverage(0);
     }
   }, [selectedCatAge, catBreeds]);
 
+  const renderDogTooltip = (breed) => (
+    <Tooltip id={`dog-${breed.id}`}>
+      Esperanza de vida promedio: {dogAgeAverage} años
+    </Tooltip>
+  );
+
+  const renderCatTooltip = (breed) => (
+    <Tooltip id={`cat-${breed.id}`}>
+      Esperanza de vida promedio: {catAgeAverage} años
+    </Tooltip>
+  );
+
   return (
-    <div className="text-center">
-      <h1 style={{ fontFamily: "Verdana, sans-serif" }}>Longevidad</h1>
-      <h6 className="border border-2 p-3 mx-4 bg-primary">
-        "En esta página se hace referencia a la esperanza de vida promedio de
+    <div className="text-center container">
+      <h1 className="mb-4" style={{ fontFamily: "Verdana, sans-serif" }}>
+        Longevidad
+      </h1>
+      <h6
+        className="border border-2 p-3 mb-4 mx-auto bg-primary text-white"
+        style={{ maxWidth: "500px" }}
+      >
+        En esta página se hace referencia a la esperanza de vida promedio de
         estos animales. Cabe mencionar que la longevidad de un perro o gato
         puede variar dependiendo de su raza, tamaño, cuidados, genética y otros
         factores. Algunas razas pueden vivir más tiempo, mientras que otras
-        tienen una esperanza de vida más corta."
+        tienen una esperanza de vida más corta.
       </h6>
+
       <Row>
         <Col>
           <DropdownButton
@@ -86,7 +168,7 @@ const BreedLongevity = () => {
             title={`Edad: Perros de ${selectedDogAge}`}
             className="mr-3"
           >
-            {dogBreeds
+            {Object.values(dogBreeds)
               .map((breed) => breed.life_span)
               .filter((value, index, self) => self.indexOf(value) === index)
               .map((age) => (
@@ -98,12 +180,20 @@ const BreedLongevity = () => {
                 </Dropdown.Item>
               ))}
           </DropdownButton>
-          {filteredDogBreeds.length > 0 && (
+          {Object.keys(filteredDogBreeds).length > 0 && (
             <div>
-              <h6>Razas de perros con longevidad de {selectedDogAge}:</h6>
-              <ListGroup className="mx-4">
-                {filteredDogBreeds.map((breed) => (
-                  <ListGroup.Item key={breed.id}>{breed.name}</ListGroup.Item>
+              <h6 className="mt-3">
+                Razas de perros con longevidad de {selectedDogAge}:
+              </h6>
+              <ListGroup>
+                {Object.values(filteredDogBreeds).map((breed) => (
+                  <OverlayTrigger
+                    key={breed.id}
+                    placement="bottom"
+                    overlay={renderDogTooltip(breed)}
+                  >
+                    <ListGroup.Item>{breed.name}</ListGroup.Item>
+                  </OverlayTrigger>
                 ))}
               </ListGroup>
             </div>
@@ -112,9 +202,10 @@ const BreedLongevity = () => {
         <Col>
           <DropdownButton
             id="catAgeDropdown"
-            title={`Edad: Gatos de ${selectedCatAge}`}
+            title={`Edades: Gatos de ${selectedCatAge}`}
+            className="ml-3"
           >
-            {catBreeds
+            {Object.values(catBreeds)
               .map((breed) => breed.life_span)
               .filter((value, index, self) => self.indexOf(value) === index)
               .map((age) => (
@@ -126,12 +217,20 @@ const BreedLongevity = () => {
                 </Dropdown.Item>
               ))}
           </DropdownButton>
-          {filteredCatBreeds.length > 0 && (
+          {Object.keys(filteredCatBreeds).length > 0 && (
             <div>
-              <h6>Razas de gatos con longevidad de {selectedCatAge}:</h6>
-              <ListGroup className="mx-4">
-                {filteredCatBreeds.map((breed) => (
-                  <ListGroup.Item key={breed.id}>{breed.name}</ListGroup.Item>
+              <h6 className="mt-3">
+                Razas de gatos con longevidad de {selectedCatAge}
+              </h6>
+              <ListGroup>
+                {Object.values(filteredCatBreeds).map((breed) => (
+                  <OverlayTrigger
+                    key={breed.id}
+                    placement="bottom"
+                    overlay={renderCatTooltip(breed)}
+                  >
+                    <ListGroup.Item>{breed.name}</ListGroup.Item>
+                  </OverlayTrigger>
                 ))}
               </ListGroup>
             </div>
