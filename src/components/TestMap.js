ff72ea5ebfe8ba3b5e846a7bbd740de29857a5db
Fernando A. Gonzalez <fagonzalez.nt@gmail.com>
2023-06-12 23:57:59 -0300

    marker and circle added

--- src/components/TestMap.js
+++ src/components/TestMap.js
@@ -1,22 +1,11 @@
 import React from "react";
 
 import GoogleMapReact from "google-map-react";
+import { Button } from "react-bootstrap";
 
 const AnyReactComponent = ({ text }) => (
-  <div
-    style={{
-      color: "white",
-      background: "red",
-      padding: "15px 10px",
-      display: "inline-flex",
-      textAlign: "center",
-      alignItems: "center",
-      justifyContent: "center",
-      borderRadius: "100%",
-      transform: "translate(-50%, -50%)",
-    }}
-  >
-    {text}
+  <div>
+    <Button>Soy Un Button</Button>
   </div>
 );
 
@@ -29,13 +18,55 @@ function TestMap() {
     zoom: 11,
   };
 
+  const getGoogleMapApi = (map, maps) => {
+    console.log("apiIsLoaded");
+    if (map) {
+      map.setOptions({
+        minZoom: 12,
+      });
+    }
+
+    if (maps) {
+      const myMarker = new maps.Marker({
+        position: { lat: -32.912915, lng: -68.855614 },
+        map,
+        draggable: true,
+        editable: true,
+        title: "Hello World!",
+      });
+
+      var myCircle = new maps.Circle({
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+        editable: true,
+        draggable: true,
+        map,
+        center: { lat: -32.912915, lng: -68.855614 },
+        radius: 10000,
+      });
+
+      myCircle.addListener("drag", () => {
+        console.log(JSON.stringify(myCircle.getCenter()));
+      });
+
+      myMarker.addListener("drag", () => {
+        console.log(JSON.stringify(myMarker.getPosition()));
+      });
+    }
+  };
+
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyBFEXdT2OetPI-ro7dq8Sy9kpVc_13oTv0" }}
+        bootstrapURLKeys={{ key: "AIzaSyCf15IY-ePezWQeCIhR9KuKWlMbtmA3bR4" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        yesIWantToUseGoogleMapApiInternals={true}
+        onGoogleApiLoaded={({ map, maps }) => getGoogleMapApi(map, maps)}
       >
         <AnyReactComponent lat={-32.912915} lng={-68.855614} text="My Marker" />
       </GoogleMapReact>
